8f3e5c3d4a757330890ad0fbacf7c20399cd3556
zokehh <milousz@wp.pl>
2022-10-14 03:38:57 +0200

    order commit

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -4,71 +4,80 @@ import CartContext from './cart-context';
 
 const defaultCartState = {
   items: [],
-  totalAmount: 0
+  totalAmount: 0,
 };
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
-    const exisitingCartItemIndex = state.items.findIndex(item => item.id === action.item.id )
-    const exisitngCartItem = state.items[exisitingCartItemIndex]
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
 
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
-    if (exisitngCartItem) {
+    if (existingCartItem) {
       const updatedItem = {
-        ...exisitngCartItem,
-        amount : exisitngCartItem.amount + action.item.amount
-      }
-      updatedItems = [
-        ...state.items
-      ]
-      updatedItems[exisitingCartItemIndex] = updatedItem
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
     } else {
-      updatedItems = {...action.item}
-      updatedItems = state.items.concat(action.item)
+      updatedItems = state.items.concat(action.item);
     }
 
-    
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
     };
   }
 
-  if (action.type === 'REMOVE') {
-    const exisitingCartItemIndex = state.items.findIndex(item => item.id === action.id )
-    const existingItem = state.items[exisitingCartItemIndex]
-    const updatedTotalAmount = state.totalAmount - existingItem.price
-
-    let updatedItems;
-
-    if (existingItem.amount === 1) {
-      updatedItems = state.items.filter(item => item.id !== action.id)
-    } else {
-      const updatedItem = {...existingItem, amount : existingItem.amount - 1}
-      updatedItems = [...state.items]
-      updatedItems[exisitingCartItemIndex] = updatedItem
-    }
-
-    return {
-      items : updatedItems,
-      totalAmount : updatedTotalAmount
-    }
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
   }
 
   return defaultCartState;
 };
 
 const CartProvider = (props) => {
-  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({type: 'ADD', item: item});
+    dispatchCartAction({ type: 'ADD', item: item });
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({type: 'REMOVE', id: id});
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
+
+  const clearCartHandler = () => {
+    dispatchCartAction({type: 'CLEAR'});
   };
 
   const cartContext = {
@@ -76,6 +85,7 @@ const CartProvider = (props) => {
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
 
   return (
