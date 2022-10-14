272abe724dbd1bec4f699a710e4481b7a1678836
zokehh <milousz@wp.pl>
2022-10-14 03:48:48 +0200

    order commit

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -49,6 +49,12 @@ const cartReducer = (state, action) => {
       updatedItems[existingCartItemIndex] = updatedItem;
     }
 
+    if (action.type === 'CLEAR') {
+      return {
+        defaultCartState
+      }
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
@@ -76,16 +82,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'REMOVE', id: id });
   };
 
-  const clearCartHandler = () => {
-    dispatchCartAction({type: 'CLEAR'});
-  };
+  const clearCartHandler = () =>  {
+    dispatchCartAction({type : 'CLEAR'})
+  }
 
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
-    clearCart: clearCartHandler
+    clearCart : clearCartHandler
   };
 
   return (
